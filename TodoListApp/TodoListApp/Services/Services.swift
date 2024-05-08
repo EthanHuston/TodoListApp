cf5be1bd5b9d39983db0b6722a497511e31fe947
Ethan Huston <71285039+EthanHuston@users.noreply.github.com>
2024-05-08 14:23:41 -0400

    Adding filter and sort settings to search query

--- TodoListApp/TodoListApp/Services/Services.swift
+++ TodoListApp/TodoListApp/Services/Services.swift
@@ -11,11 +11,41 @@ class Services {
     static let shared = Services()
 
     private let baseURL = URL(string: "http://localhost:3000")!
+    
+    var filterSetting: filterOptions = .all
+    var sortBySetting: sortByOptions = .due
+    var sortDirectionSetting: sortDirectionOptions = .ascending
 
     private init() {}
 
     func fetchTasks(completion: @escaping ([Task]?) -> Void) {
         let url = baseURL.appendingPathComponent("/tasks")
+        
+        var components = URLComponents(url: baseURL.appendingPathComponent("/tasks"), resolvingAgainstBaseURL: true)
+        var queryItems = [URLQueryItem]()
+        
+        if filterSetting != .all {
+            if filterSetting == .complete {
+                queryItems.append(URLQueryItem(name: "completed", value: "true"))
+            } else {
+                queryItems.append(URLQueryItem(name: "completed", value: "false"))
+            }
+        }
+        
+        if sortBySetting == .due {
+            if sortDirectionSetting == .ascending {
+                queryItems.append(URLQueryItem(name: "sort_by", value: "+dueDate"))
+            } else {
+                queryItems.append(URLQueryItem(name: "sort_by", value: "-dueDate"))
+            }
+        } else {
+            if sortDirectionSetting == .ascending {
+                queryItems.append(URLQueryItem(name: "sort_by", value: "+createdDate"))
+            } else {
+                queryItems.append(URLQueryItem(name: "sort_by", value: "-createdDate"))
+            }
+        }
+        
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data else {
                 print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
